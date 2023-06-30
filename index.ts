e1cf609ff93d728dbe68e89f7b15d78959b7b109
Matias Olivera <moliverafreire@gmail.com>
2023-06-30 15:18:49 -0400

    Move index.ts inside src

--- index.ts
+++
@@ -1,6 +0,0 @@
-export * from './src/addresses';
-export * from './src/call';
-export * from './src/messages';
-export * from './src/provider';
-export * from './src/transactions';
-export * from './src/types';
---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from './addresses';
+export * from './call';
+export * from './messages';
+export * from './provider';
+export * from './transactions';
+export * from './types';
