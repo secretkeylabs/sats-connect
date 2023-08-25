a48fd76f32230589dde75e3e99600295e09a51fc
victorkirov <victor.kirov@gmail.com>
2023-08-25 15:40:02 +0300

    Add capabilities function

--- src/provider/types.ts
+++ src/provider/types.ts
@@ -1,10 +1,11 @@
 import type { GetAddressResponse } from '../addresses';
 import type { CallWalletResponse } from '../call';
+import type { GetCapabilitiesResponse } from '../capabilities';
 import type { CreateFileInscriptionResponse, CreateTextInscriptionResponse } from '../inscriptions';
 import type { SignMessageResponse } from '../messages';
 import type { SendBtcTransactionResponse, SignTransactionResponse } from '../transactions';
 
-export interface BitcoinProvider {
+interface BaseBitcoinProvider {
   call: (request: string) => Promise<CallWalletResponse>;
   connect: (request: string) => Promise<GetAddressResponse>;
   signMessage: (request: string) => Promise<SignMessageResponse>;
@@ -12,6 +13,12 @@ export interface BitcoinProvider {
   sendBtcTransaction: (request: string) => Promise<SendBtcTransactionResponse>;
   createTextInscription: (request: string) => Promise<CreateTextInscriptionResponse>;
   createFileInscription: (request: string) => Promise<CreateFileInscriptionResponse>;
+}
+
+export type Capability = keyof BaseBitcoinProvider;
+
+export interface BitcoinProvider extends BaseBitcoinProvider {
+  getCapabilities: (request: string) => Promise<GetCapabilitiesResponse>;
 }
 
 declare global {
