848ae60295d7f9d4f9cec9d55c29ea24c4cf5671
Mahmoud Aboelenein <mahmoud@secretkeylabs.com>
2024-01-24 14:51:49 +0200

    ENG-3372 - [Get Address] Add a "purposes" option to retrieve the user's Stacks address (#55)
    * added stx to getAddress purposes
    
    * rename stacks purpose in getAddress
    
    * fix typo
    
    * add addressType to getAddress response
    
    * change enum name

--- src/addresses/types.ts
+++ src/addresses/types.ts
@@ -3,6 +3,7 @@ import type { RequestOptions, RequestPayload } from '../types';
 export enum AddressPurpose {
   Ordinals = 'ordinals',
   Payment = 'payment',
+  Stacks = 'stacks',
 }
 
 export interface GetAddressPayload extends RequestPayload {
@@ -10,10 +11,20 @@ export interface GetAddressPayload extends RequestPayload {
   message: string;
 }
 
+export enum AddressType {
+  p2pkh = 'p2pkh',
+  p2sh = 'p2sh',
+  p2wpkh = 'p2wpkh',
+  p2wsh = 'p2wsh',
+  p2tr = 'p2tr',
+  stacks = 'stacks',
+}
+
 export interface Address {
   address: string;
   publicKey: string;
   purpose: AddressPurpose;
+  addressType?: AddressType;
 }
 
 export interface GetAddressResponse {
