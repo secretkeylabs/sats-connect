4c4c2732788ecf0cb95494dc72196db2f26d58ed
Tim Man <tim@secretkeylabs.com>
2024-06-28 18:42:34 +0800

    chore: trigger a netlify build

--- example/src/components/MintRunes/index.tsx
+++ example/src/components/MintRunes/index.tsx
@@ -38,7 +38,7 @@ const MintRunes = ({ addresses, network }: Props) => {
       setTotalSize(response.result.totalSize);
     } else {
       console.error(response.error);
-      alert('Error Fetching Estimate. See console for details.');
+      alert(`Error estimating ${runeName} mint. See console for details.`);
     }
   };
 
