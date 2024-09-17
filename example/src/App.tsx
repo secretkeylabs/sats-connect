6fffccb5168c10ca6c19b140d53266e83be1b571
Eduard Bardají Puig <ebardaji@gmail.com>
2024-09-17 23:44:16 +0100

    Add disconnect events

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -60,15 +60,36 @@ function AppWithProviders({ children }: React.PropsWithChildren<{}>) {
 
   const isConnected = btcAddressInfo.length + stxAddressInfo.length > 0;
 
+  const clearAppData = useCallback(() => {
+    setBtcAddressInfo([]);
+    setStxAddressInfo([]);
+    queryClient.clear();
+  }, [queryClient, setBtcAddressInfo, setStxAddressInfo]);
+
+  const onDisconnect = useCallback(() => {
+    (async () => {
+      await Wallet.disconnect();
+      clearAppData();
+    })().catch(console.error);
+  }, [queryClient, setBtcAddressInfo, setStxAddressInfo]);
+
   useEffect(() => {
     if (btcAddressInfo.length < 1) return;
 
-    const removeListener = Wallet.addListener('accountChange', (ev) => {
+    const removeListenerAccountChange = Wallet.addListener('accountChange', (ev) => {
       console.log('The account has changed.', ev);
     });
 
-    return removeListener;
-  });
+    const removeListenerDisconnect = Wallet.addListener('disconnect', (ev) => {
+      console.log('The wallet has been disconnected. Event:', ev);
+      clearAppData();
+    });
+
+    return () => {
+      removeListenerAccountChange();
+      removeListenerDisconnect();
+    };
+  }, [btcAddressInfo]);
 
   const onConnectLegacy = useCallback(() => {
     (async () => {
@@ -111,15 +132,6 @@ function AppWithProviders({ children }: React.PropsWithChildren<{}>) {
       setStxAddressInfo(res3.result.addresses);
     })().catch(console.error);
   }, [setBtcAddressInfo, setStxAddressInfo]);
-
-  const onDisconnect = useCallback(() => {
-    (async () => {
-      await Wallet.disconnect();
-      setBtcAddressInfo([]);
-      setStxAddressInfo([]);
-      queryClient.clear();
-    })().catch(console.error);
-  }, [queryClient, setBtcAddressInfo, setStxAddressInfo]);
 
   const connectionContextValue = useMemo(
     () => ({ network, btcAddressInfo, stxAddressInfo, onDisconnect }),
