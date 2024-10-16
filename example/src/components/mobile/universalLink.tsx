24ecedfccbb2cdd43808989fb8ff10f1f6e590ab
fede erbes <fedeerbes@gmail.com>
2024-10-16 12:28:03 +0200

    chore: add url input

--- example/src/components/mobile/universalLink.tsx
+++ example/src/components/mobile/universalLink.tsx
@@ -1,12 +1,13 @@
-import { Button, Card } from '../../App.styles';
+import { useState } from 'react';
+import { Button, Card, Input } from '../../App.styles';
 
 export function MobileUniversalLink() {
+  const [url, setUrl] = useState(
+    'https://connect.xverse.app/browser?url=wallet.xverse.app/explore',
+  );
+
   const handleClick = () => {
-    window.open(
-      'https://connect.xverse.app/browser?url=wallet.xverse.app/explore',
-      '_blank',
-      'noopener,noreferrer',
-    );
+    window.open(url, '_blank', 'noopener,noreferrer');
   };
 
   return (
@@ -16,6 +17,10 @@ export function MobileUniversalLink() {
         If Xverse app is installed, it will open the browser withing the app using the url param
       </div>
       <div>If Xverse app is not installed, it will prompt the user to install the app</div>
+      <div style={{ marginTop: 15, marginBottom: 15 }}>
+        <div>url param</div>
+        <Input type="text" value={url} onChange={(e) => setUrl(e.target.value)} />
+      </div>
       <Button onClick={handleClick}>Navigate to Xverse Explore</Button>
     </Card>
   );
