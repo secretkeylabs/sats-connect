3eea7b8dd28bfabe998bd562e6b877fea0640392
Edu <ebardaji@gmail.com>
2025-05-26 18:03:33 +0100

    Improve connection behavior (#226)
    * Improve connection behavior
    
    * Add missing deps on useEffect
    
    * Remove unnecessary log

---
+++ example/src/components/GlobalStateProvider/use-global-state.tsx
@@ -0,0 +1,10 @@
+import { useContext } from 'react';
+import { GlobalStateContext } from './context';
+
+export function useGlobalState() {
+  const context = useContext(GlobalStateContext);
+  if (context === undefined) {
+    throw new Error('useGlobalState must be used within a GlobalStateProvider');
+  }
+  return context;
+}
