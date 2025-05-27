b7467ac06ca419186dfa885a2b876eb82bfff6b3
Tim Man <tim@secretkeylabs.com>
2025-05-27 13:18:14 +0800

    docs: clean up wallet_connect method

--- example/src/components/wallet/WalletConnect.tsx
+++ example/src/components/wallet/WalletConnect.tsx
@@ -1,76 +1,87 @@
-import { Button, Card, NativeSelect } from '@mantine/core';
+import { ActionIcon, Button, Card, Code, NativeSelect } from '@mantine/core';
+import { IconExternalLink } from '@tabler/icons-react';
 import { useState } from 'react';
-import { AddressPurpose, BitcoinNetworkType, request } from 'sats-connect';
+import { AddressPurpose, BitcoinNetworkType, request, type ConnectParams } from 'sats-connect';
+import styled from 'styled-components';
+
+const MethodHeading = styled.h3({
+  display: 'flex',
+  alignItems: 'center',
+  a: { marginLeft: '1rem' },
+});
+
+const TwoColGrid = styled.div({
+  display: 'grid',
+  gridTemplateColumns: '1fr 1fr',
+  gap: '1rem',
+});
+
+const FormDiv = styled.div({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: '1rem',
+  marginTop: '1rem',
+});
 
 export const WalletConnect = () => {
-  const [desiredNetwork, setDesiredNetwork] = useState<BitcoinNetworkType>(
-    BitcoinNetworkType.Mainnet,
-  );
   const [response, setResponse] = useState<string | null>(null);
+  const [options, setOptions] = useState<ConnectParams>({
+    message: 'Optional message displayed on connection popup',
+    addresses: [AddressPurpose.Payment, AddressPurpose.Ordinals, AddressPurpose.Stacks],
+    network: BitcoinNetworkType.Mainnet,
+  });
 
   const handleWalletConnect = () => {
     const handler = async () => {
       const method = 'wallet_connect';
-      const options = {
-        message: 'Cool app wants to know your addresses!',
-        addresses: [AddressPurpose.Payment, AddressPurpose.Ordinals, AddressPurpose.Stacks],
-        network: desiredNetwork,
-      };
-      console.log(`called request("${method}") with options:`);
-      console.log(options);
       const res = await request(method, options);
       setResponse(JSON.stringify(res, null, 2));
+      console.log('request("wallet_connect", options)');
+      console.log('options:\n', options);
+      console.log('response:\n', res);
 
       if (res.status === 'error') {
         console.error('wallet_connect error');
-        console.error(res);
         return;
       }
     };
     handler().catch(console.error);
   };
+
   return (
     <Card>
-      <h3>wallet_connect</h3>
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: '1fr 1fr',
-          gap: '1rem',
-        }}
-      >
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'column',
-            gap: '1rem',
-          }}
+      <MethodHeading>
+        wallet_connect
+        <ActionIcon
+          component="a"
+          href="https://docs.xverse.app/sats-connect/connecting-to-the-wallet/connect-to-xverse-wallet"
+          target="_blank"
+          variant="transparent"
+          size="sm"
         >
-          <NativeSelect
-            label={'network'}
-            onChange={(e) => setDesiredNetwork(e.target.value as BitcoinNetworkType)}
-          >
-            <option value={BitcoinNetworkType.Mainnet}>{BitcoinNetworkType.Mainnet}</option>
-            <option value={BitcoinNetworkType.Testnet}>{BitcoinNetworkType.Testnet}</option>
-            <option value={BitcoinNetworkType.Testnet4}>{BitcoinNetworkType.Testnet4}</option>
-            <option value={BitcoinNetworkType.Signet}>{BitcoinNetworkType.Signet}</option>
-            <option value={BitcoinNetworkType.Regtest}>{BitcoinNetworkType.Regtest}</option>
-          </NativeSelect>
-          <Button onClick={handleWalletConnect}>request</Button>
+          <IconExternalLink />
+        </ActionIcon>
+      </MethodHeading>
+      <TwoColGrid>
+        <div>
+          <h4>Options</h4>
+          <Code block>{JSON.stringify(options, null, 2)}</Code>
+          <FormDiv>
+            <NativeSelect
+              label={'network'}
+              data={Object.values(BitcoinNetworkType)}
+              onChange={(e) =>
+                setOptions((prev) => ({ ...prev, network: e.target.value as BitcoinNetworkType }))
+              }
+            />
+            <Button onClick={handleWalletConnect}>request</Button>
+          </FormDiv>
         </div>
         <div>
           <h4>Response</h4>
-          <pre
-            style={{
-              maxHeight: 800,
-              fontSize: 'x-small',
-              overflowY: 'auto',
-            }}
-          >
-            {response ? response : 'No response yet'}
-          </pre>
+          <Code block>{response}</Code>
         </div>
-      </div>
+      </TwoColGrid>
     </Card>
   );
 };
