7d68444f8cb2eeb1ba49f0ab025848cd2dda0242
Eduard Bardají Puig <ebardaji@gmail.com>
2025-08-26 13:06:13 +0100

    Handle legacy addListener

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import {
   BaseAdapter,
+  ListenerInfo,
   Params,
   Requests,
   RpcErrorCode,
@@ -109,7 +110,18 @@ class Wallet {
     return response;
   }
 
-  public addListener: AddListener = (listenerInfo) => {
+  public addListener: AddListener = (...rawArgs) => {
+    const listenerInfo: ListenerInfo = (() => {
+      if (rawArgs.length === 1) return rawArgs[0];
+
+      // Assume legacy request with 2 params
+      const actualArgs: unknown[] = rawArgs;
+      return {
+        eventName: actualArgs[0],
+        cb: actualArgs[1],
+      } as ListenerInfo;
+    })();
+
     const defaultProvider = getDefaultProvider();
     if (!this.isProviderSet() && defaultProvider) {
       this.providerId = defaultProvider;
